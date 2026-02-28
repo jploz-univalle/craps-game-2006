9259566d62c7799d46758f2d2cc1fc2d314dc44a
Issho31 <jdiego.16c@gmail.com>
2026-02-28 16:56:11 -0500

    Added new GUI

--- src/main/java/com/example/crapsgame2006/HelloApplication.java
+++ src/main/java/com/example/crapsgame2006/HelloApplication.java
@@ -2,7 +2,11 @@ package com.example.crapsgame2006;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +15,15 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Parent root = fxmlLoader.load();
+//        VBox root = new VBox();
+//        Button helloButton = new Button("Saludar!");
+//        Label helloLabel = new Label("Clickea aqui:");
+//        root.getChildren().add(helloButton);
+//        root.getChildren().add(helloLabel);
+
+        Scene scene = new Scene(root);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
